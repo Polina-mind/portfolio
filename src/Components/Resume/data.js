d1350209b969d22a732848223ef7b3cf78ba00cd
Polina Artiushenko <Polina_Artiushenko@epam.com>
2024-03-06 00:33:32 +0200

    Improve code

---
+++ src/Components/Resume/data.js
@@ -0,0 +1,75 @@
+const data = {
+  name: "Polina Artiushenko",
+  position: "SOFTWARE ENGINEER",
+  contacts: {
+    phone: "+38(099)797-55-15",
+    mail: "artiushenkopolin@gmail.com",
+    place: "Kyiv, Ukraine",
+    linkedin: "https://www.linkedin.com/in/polina1505",
+  },
+  profileSummary:
+    "Software Engineer with more than 2 years of experience, devoted to JS & React & Team.",
+  professionalSkills: [
+    "Result-driven",
+    "Team player",
+    "Critical-thinking",
+    "Responsible",
+    "Communicative",
+    "Work ethic",
+    "Positive",
+  ],
+  technicalSkills: [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React",
+    "Node.js",
+    "Git",
+    "Jest",
+    "WDIO",
+    "VS Code",
+    "JAWS",
+    "Jira",
+    "Jenkins",
+  ],
+  workExperience: [
+    {
+      jobTitle: "DEVELOPER",
+      company: "EPAM",
+      period: "Sep 2021 - Present",
+      description: [
+        "Working with web technologies HTML, CSS, JavaScript, React, and various libraries.",
+        "Bugs fixing in application modules.",
+        "Developing React components to replace the existing jQuery code.",
+        "Utilizing testing tools, including Jest, WDIO (using Docker and Rancher), and TestCafe, to test web applications.",
+        "Enhancing accessibility of web applications to provide equal user access (working with JAWS screen reader).",
+        "Effective communication with clients regarding PR's review.",
+        "Working with tools, including Jira, Jenkins, and GitHub.",
+        "Providing guidance for newcomers during the onboarding period.",
+      ],
+    },
+    {
+      jobTitle: "DEVELOPER",
+      company: "GoIT",
+      period: "Feb 2021 - Aug 2021",
+      description: [
+        "Utilized agile development methodologies to ensure project success.",
+        "Creating the ability to add|delete the movie to the user's library, implemented with local storage.",
+        "Creating markup with responsive design.",
+      ],
+    },
+  ],
+  education: [
+    {
+      degree: "MASTER's DEGREE IN COMPUTER SCIENCE",
+      school: "NTUU 'KPI'",
+      year: "2008",
+    },
+  ],
+  additionalInformation: [
+    "ENGLISH level B2",
+    "Oracle Cloud Infrastructure Foundations Certified Associate",
+  ],
+};
+
+export default data;
