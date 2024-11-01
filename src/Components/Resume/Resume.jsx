704d85027fa7ec7b9ac0298edeb5900dafae6618
Polina-mind <popova.v.polina@gmail.com>
2024-11-01 23:36:58 +0200

    Update Resume component

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { Fragment } from "react";
 import resumeData from "./data";
 import "./Resume.css";
 
@@ -14,79 +14,96 @@ const Resume = () => (
           </a>
         </li>
         <li>
+          <a className="contact" href={resumeData.contacts.linkedin}>
+            linkedIn
+          </a>
+        </li>
+        <li>
           <a className="contact" href={`mailto:${resumeData.contacts.mail}`}>
             {resumeData.contacts.mail}
           </a>
         </li>
         <li>{resumeData.contacts.place}</li>
-        <li>
-          <a className="contact" href={resumeData.contacts.linkedin}>
-            {resumeData.contacts.linkedin}
-          </a>
-        </li>
       </ul>
     </div>
 
-    <div className="block">
+    <div>
       <h2 className="text-on-line">PROFILE SUMMARY</h2>
       <p className="text">{resumeData.profileSummary}</p>
     </div>
 
-    <div className="block">
+    <div>
       <h2 className="text-on-line">PROFESSIONAL SKILLS</h2>
       <ul className="list">
-        {resumeData.professionalSkills.map((skill, index) => (
-          <li key={index}>{skill}</li>
+        {resumeData.professionalSkills.map((skill) => (
+          <li key={skill}>{skill}</li>
         ))}
       </ul>
     </div>
 
-    <div className="block">
+    <div>
       <h2 className="text-on-line">TECHNICAL SKILLS</h2>
       <ul className="list">
-        {resumeData.technicalSkills.map((skill, index) => (
-          <li key={index}>{skill}</li>
+        {resumeData.technicalSkills.map((skill) => (
+          <li key={skill}>{skill}</li>
         ))}
       </ul>
     </div>
 
-    <div className="block">
+    <div>
+      <h2 className="text-on-line">MY PROJECTS</h2>
+      <ul className="list">
+        {resumeData.myProjects.map(({ id, name, link }) => (
+          <li key={id}>
+            <a className="contact" href={link}>
+              {name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+
+    <div>
       <h2 className="text-on-line">WORK EXPERIENCE</h2>
-      {resumeData.workExperience.map((job, index) => (
-        <>
-          <div className="job" key={index}>
-            <h3 className="job-title">{job.jobTitle}</h3>
-            <div className="job-info">
-              <p className="company">{job.company}</p>
-              <p>{job.period}</p>
+      {resumeData.workExperience.map(
+        ({ jobTitle, project, company, period, description }) => (
+          <Fragment key={project}>
+            <div className="job">
+              <h3 className="job-title">
+                {jobTitle} in {project} project
+              </h3>
+              <div className="job-info">
+                <p className="company">{company}</p>
+                <p>{period}</p>
+              </div>
             </div>
-          </div>
-          <ul className="text job-description">
-            {job.description.map((desc, index) => (
-              <li key={index}>{desc}</li>
-            ))}
-          </ul>
-        </>
-      ))}
+            <ul className="text job-description">
+              {description.map((desc, index) => (
+                <li key={index}>{desc}</li>
+              ))}
+            </ul>
+          </Fragment>
+        )
+      )}
     </div>
 
     <div className="block">
       <h2 className="text-on-line">EDUCATION</h2>
-      {resumeData.education.map((edu, index) => (
-        <div key={index}>
-          <p className="university">{edu.degree}</p>
+      {resumeData.education.map(({ degree, school, year }) => (
+        <div key={school}>
+          <p className="university">{degree}</p>
           <div className="university-info">
-            <p className="company">{edu.school}</p>
-            <p>{edu.year}</p>
+            <p className="company">{school}</p>
+            <p>{year}</p>
           </div>
         </div>
       ))}
     </div>
 
-    <div className="block">
+    <div>
       <h2 className="text-on-line">ADDITIONAL INFORMATION</h2>
-      {resumeData.additionalInformation.map((info, index) => (
-        <p className="add-info" key={index}>
+      {resumeData.additionalInformation.map((info) => (
+        <p className="add-info" key={info}>
           {info}
         </p>
       ))}
